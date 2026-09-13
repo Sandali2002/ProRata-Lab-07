15ffe2ff7e5a9293c6e5f80b6213f484699ae6df
Tharuka Nishshanka <sandalitharukan@gmail.com>
2026-09-14 00:07:43 +0530

    Add files via upload

---
+++ Q2/IT23376472Lab7Q2A.java
@@ -0,0 +1,13 @@
+public class IT23376472Lab7Q2A {
+    public static void main(String[] args) {
+	
+	   for (int i = 0; i < 4; i++) {
+	        for (int j = 0; j < 5; j++) {
+			     System.out.print("$" +" ");
+			}
+			
+			System.out.println();
+	   }
+	   
+	}
+}
